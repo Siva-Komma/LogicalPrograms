50e0c1e1553d3d8a90eea6d73d2e4f05e7bf8594
Siva-Komma <kommasivajyothi22@gmail.com>
2023-02-16 16:19:52 +0530

    Checking week of day for the month and year

--- LogicalProgramming/DayofWeek.cs
+++ LogicalProgramming/DayofWeek.cs
@@ -11,7 +11,7 @@ namespace LogicalProgramming
         public static void CheckingDayofWeek()
         {
             int d=0,y=0,m=0,d2=0;
-            double y0 = 0, x = 0, m0 = 0,d0=0;
+            int y0 = 0, x = 0, m0 = 0,d0=0;
             Console.WriteLine("enter day");
             d = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("enter month");
@@ -22,9 +22,9 @@ namespace LogicalProgramming
             y0 = y - (14 - m) / 12;
             x = y0 + y0 / 4 - y0 / 100 + y0 / 400;
             m0 = m + 12 * ((14 - m) / 12) - 2;
-            d0 = (d + x + 31 * m0 / 12) % 7;
-            d2 = Convert.ToInt32(d0);
-            switch(d2)
+            d0 =(d + x + 31 * m0 / 12) % 7;
+
+            switch(d0)
             {
                 case 0:
                     Console.WriteLine("Sunday");
